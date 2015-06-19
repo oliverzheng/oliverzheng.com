4d5babeb655a5c77638fc8c67b6e6a64ee480f85
Oliver Zheng <gitcommits@oliverzheng.com>
2015-06-19 00:20:07 -0500

    Add indexing of blog articles.

---
+++ build.js
@@ -0,0 +1,30 @@
+var handlebars = require('handlebars');
+var handlebarsIntl = require('handlebars-intl');
+handlebarsIntl.registerWith(handlebars);
+
+var Metalsmith = require('metalsmith');
+var markdown = require('metalsmith-markdown');
+var paths = require('metalsmith-paths');
+var permalinks = require('metalsmith-permalinks');
+var templates = require('metalsmith-templates');
+var assets = require('metalsmith-assets');
+var collections = require('metalsmith-collections');
+
+Metalsmith(__dirname)
+  .use(collections({
+    articles: {
+    }
+  }))
+  .use(markdown())
+  .use(paths())
+  .use(permalinks({
+    pattern: ':collection/:title',
+  }))
+  .use(templates('handlebars'))
+  .use(assets({
+    source: './assets',
+    destination: './assets',
+  }))
+  .build(function(err) {
+    if (err) throw err;
+  });
--- src/index.md
+++ src/index.md
@@ -2,10 +2,6 @@
 template: index.html
 ---
 
-# Oliver Zheng
-
-I'm a software engineer at Facebook. Here's some stuff not about that.
-
 ## Projects
 
 - [File Github issues over Twitter][]
